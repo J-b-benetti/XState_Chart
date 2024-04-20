57bcd1a2bb161e42faccfb6283cc45ac1b15b958
J-b-benetti <j.b.benetti81@gmail.com>
2024-04-20 12:32:38 +0200

    Implementation of execute(), canUndo(), canRedo() methods

--- pages/polylineUndo/undoManager.js
+++ pages/polylineUndo/undoManager.js
@@ -1,2 +1,23 @@
-import Commande from "./command";
+import Stack from "./stack";
 
+class UndoManager {
+    constructor() {
+        this.undoStack = new Stack;
+        this.redoStack = new Stack;
+    }
+
+    execute(commande) {
+        commande.execute();
+        this.undoStack.push(commande);
+        this.redoStack = [];
+    }
+
+    canUndo() {
+        return !this.undoStack.isEmpty();
+    }
+
+    candRedo() {
+        return !this.redoStack.isEmpty();
+        
+    }
+}
